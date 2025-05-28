cf15fe3b6c8aaf7119c8fe969bee0c569754064d
pbouille <75848998+pbouille@users.noreply.github.com>
2025-05-28 09:36:59 +0200

    mise a jour
    mise a jour

---
+++ Niovelius-Software-EngFr/Niovelius-Software-EngFr/Niovelius-Software-EngFr/international/niovelius.js
@@ -0,0 +1,11 @@
+window.myLanguages =
+[
+    {"Language":"English"},
+    {"Language":"Francais"},
+    {"Language":"Espanol"},
+    {"Language":"Italiano"},
+    {"Language":"Portugues"},
+    {"Language":"Suomi"},
+    {"Language":"Norsk"},
+    {"Language":"Nederlands"}
+];
